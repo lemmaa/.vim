2b18ad31b9e415758d0084ec00fe0b44b8eedd6a
Sung-Jae Lee <sj925.lee@samsung.com>
2019-02-08 10:15:08 +0900

    Update install script
    - Add `fzf` install

--- install.sh
+++ install.sh
@@ -4,10 +4,31 @@ VIM_BASE=${HOME}/.vim
 AUTOLOAD_PATH=${VIM_BASE}/autoload
 BUNDLE_PATH=${VIM_BASE}/bundle
 
+#
+# install plug-ins
+#
 git submodule update --init --recursive
 
+#
+# install fzf
+#
+if [ -d ~/.fzf ]; then
+	git -C ~/.fzf pull --rebase
+else
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+fi
+	~/.fzf/install --all
+
+#
+# initialize YCM plug-in
+# - compile language support module
+#
 pushd ${BUNDLE_PATH}/YouCompleteMe
 ./install.py --all
-ln -s ${VIM_BASE}/.vimrc ${HOME}/.vimrc
 popd
+
+#
+# create a symbolic link of `.vimrc` at $HOME
+#
+ln -s ${VIM_BASE}/.vimrc ${HOME}/.vimrc
 
