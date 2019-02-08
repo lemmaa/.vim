18539ccc635a89726efe78766b627c18c18d0dcc
Sung-Jae Lee <sj925.lee@samsung.com>
2019-02-08 12:11:43 +0900

    Improve install script
    - Add install prerequisities of YCM
    - Force link .vimrc

--- install.sh
+++ install.sh
@@ -13,16 +13,24 @@ git submodule update --init --recursive
 # install fzf
 #
 if [ -d ~/.fzf ]; then
-	git -C ~/.fzf pull --rebase
+  git -C ~/.fzf pull --rebase
 else
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 fi
-	~/.fzf/install --all
+  ~/.fzf/install --all
 
 #
 # initialize YCM plug-in
 # - compile language support module
 #
+HOST_OS=$(uname -s)
+if [ "${HOST_OS}"=="Linux" ]; then
+  sudo apt --yes install nodejs npm mono-devel
+elif [ "${HOST_OS}"=="Darwin" ]; then
+  brew update && brew install node.js npm mono
+fi
+# install rust cargo
+command -v cargo > /dev/null 2>&1 || (curl https://sh.rustup.rs -sSf | sh)
 pushd ${BUNDLE_PATH}/YouCompleteMe
 ./install.py --all
 popd
@@ -30,5 +38,6 @@ popd
 #
 # create a symbolic link of `.vimrc` at $HOME
 #
+rm -f ${HOME}/.vimrc
 ln -s ${VIM_BASE}/.vimrc ${HOME}/.vimrc
 
